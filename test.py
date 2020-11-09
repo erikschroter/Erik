8a2d4a5159299b628b26094619fb6ce42cf9cbc2
ChristophPabsch <cmpabsch@gmail.com>
2020-11-09 17:07:57 +0100

    Update

--- test.py
+++ test.py
@@ -1,8 +1,30 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Mon Nov  9 14:45:31 2020
+import numpy as np
+import scipy as sp
+from scipy import integrate
+import matplotlib.pyplot as plt
+dt = 0.1
+xlist = [0]
+flist = [0]
+ilist = [0]
+def f(x):
+    if x<0:
+        return 0
+    if x>=0 and x < 2:
+        return x**2
+    if x>=2 and x< 4:
+        return 6-x
+    if x>=4 and x<=5:
+        return (-2*x**3)/61+(250/61)
+    if x>=5:
+        return 0
 
-@author: michi
-"""
+while xlist[-1]<=10:
+    xlist.append(xlist[-1]+dt)
+    flist.append(f((xlist[-1])))
+    ilist.append(sp.integrate.quad(f,0,xlist[-1])[0])
 
-print("testing")
+
+plt.plot(xlist,flist,'r')
+plt.plot(xlist,ilist,'b')
+
+plt.show()
