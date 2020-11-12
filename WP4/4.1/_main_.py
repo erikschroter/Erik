ce52eaf43ce15255720ad2d6536f08f9798bace0
michielvandm <michielvandermarliere@hotmail.com>
2020-11-12 17:01:52 +0100

    getting there

--- WP4/4.1/_main_.py
+++ WP4/4.1/_main_.py
@@ -1,6 +1,12 @@
 
 #define constants
 
+filename = 'MainWing_a0.00_v10.00ms.csv'
+rho = 1.225
+v = 70
+span = 69.92
+accuracy = 41 
+
 #Maximum takeoff weight [kg]
 MTOW = 291_509.2
 
@@ -18,3 +24,12 @@ W_uc_MLG = 7_569.349
 
 #Wing weight including mounts and spoilers [kg]
 WingWeight = 3210.55
+
+from intigrate import *
+from interpolate import *
+
+
+f, xdist = liftdistribution(filename, rho, v, span, accuracy)
+
+DrawShearForce(f,xdist)
+DrawBendingMoment(f,xdist)
