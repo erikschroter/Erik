b097462ed30925bbeffbe99c859482fae7d398ff
PhilipVaartjes <PhilipVaartjes@users.noreply.github.com>
2020-11-12 15:17:30 +0100

    Update interpolate.py

--- WP4/4.1/interpolate.py
+++ WP4/4.1/interpolate.py
@@ -3,26 +3,33 @@ import numpy as np
 import matplotlib.pyplot as plt
 from ReadingXFLRresults import ReadingXFLR
 
+#setting variables for wing dimensions and flight conditions
+rho=0.4416
+v=203.720
+S=543.25
+
 #The code below fetches the various aerodynamic data from the XFLR analysis
 CL, yspan, Chord, Ai, Cl, ICd, CmAirfquarterchord = ReadingXFLR('MainWing_a0.00_v10.00ms.csv')
+print(Cl)
 
 #determining what variables need to be on the x, and y -axis
 x = yspan
-y = Cl
-
-print(x)
-print(y)
+lst=Cl
+for i in range(len(Cl)-1):
+    lst[i] = 0.5*rho*(v**2)*Chord[i]*Cl[i]
+y=lst
 
 #interpolating the data in a cubic manner
-f = interp1d(x,y,kind="cubic")
+f = interp1d(x,y,kind="cubic", fill_value="extrapolate")
 
 #determining over which range the interpolation needs to be determined, here num= determines the accuracy of the interpolation.
-xnew = np.linspace(x[0], x[len(x)-1], num=41, endpoint=True)
+xnew = np.linspace(0, 34.96, num=41, endpoint=True)
+
 
 #plotting the datapoints and interpolation because it looks nice
 plt.plot(x,y,"o", xnew, f(xnew), "-")
 plt.show()
 
 #If an exact value needs to be known you can determine it here
-print(f(2.3))
-
+print(Cl)
+print(Chord)
