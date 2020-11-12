7bd6cfcfd08d3ff4c912011eed29282db7ffc2f0
PhilipVaartjes <PhilipVaartjes@users.noreply.github.com>
2020-11-12 16:05:04 +0100

    Update interpolate.py

--- WP4/4.1/interpolate.py
+++ WP4/4.1/interpolate.py
@@ -1,35 +1,37 @@
 from scipy.interpolate import interp1d
+import scipy as sp
+from scipy import integrate
 import numpy as np
 import matplotlib.pyplot as plt
 from ReadingXFLRresults import ReadingXFLR
 
-#setting variables for wing dimensions and flight conditions
-rho=0.4416
-v=203.720
-S=543.25
 
-#The code below fetches the various aerodynamic data from the XFLR analysis
-CL, yspan, Chord, Ai, Cl, ICd, CmAirfquarterchord = ReadingXFLR('MainWing_a0.00_v10.00ms.csv')
-print(Cl)
+def liftdistribution(file):
+    #setting variables for wing dimensions and flight conditions
+    rho=1.225
+    v=70
 
-#determining what variables need to be on the x, and y -axis
-x = yspan
-lst=Cl
-for i in range(len(Cl)-1):
-    lst[i] = 0.5*rho*(v**2)*Chord[i]*Cl[i]
-y=lst
+    #The code below fetches the various aerodynamic data from the XFLR analysis
+    CL, yspan, Chord, Ai, Cl, ICd, CmAirfquarterchord = ReadingXFLR(file)
+    print(Cl)
 
-#interpolating the data in a cubic manner
-f = interp1d(x,y,kind="cubic", fill_value="extrapolate")
+    #determining what variables need to be on the x, and y -axis
+    x = yspan
+    lst=Cl
+    for i in range(len(Cl)-1):
+        lst[i] = 0.5*rho*(v**2)*Chord[i]*Cl[i]
+    y=lst
 
-#determining over which range the interpolation needs to be determined, here num= determines the accuracy of the interpolation.
-xnew = np.linspace(0, 34.96, num=41, endpoint=True)
+    #interpolating the data in a cubic manner
+    f = interp1d(x,y,kind="cubic", fill_value="extrapolate")
 
+    #determining over which range the interpolation needs to be determined, here num= determines the accuracy of the interpolation.
+    xnew = np.linspace(0, 34.96, num=41, endpoint=True)
 
-#plotting the datapoints and interpolation because it looks nice
-plt.plot(x,y,"o", xnew, f(xnew), "-")
-plt.show()
+    #plotting the datapoints and interpolation because it looks nice
+    plt.plot(x,y,"o", xnew, f(xnew), "-")
+    plt.show()
 
-#If an exact value needs to be known you can determine it here
-print(Cl)
-print(Chord)
+    return f
+
+liftdistribution('MainWing_a0.00_v10.00ms.csv')
