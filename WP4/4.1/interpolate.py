b5281dc31180064cfe0bc61126c8c2e775201e20
PhilipVaartjes <PhilipVaartjes@users.noreply.github.com>
2020-11-12 16:50:20 +0100

    Update interpolate.py

--- WP4/4.1/interpolate.py
+++ WP4/4.1/interpolate.py
@@ -6,10 +6,10 @@ import matplotlib.pyplot as plt
 from ReadingXFLRresults import ReadingXFLR
 
 
-def liftdistribution(file):
+def liftdistribution(file, rho, v, span, accuracy):
     #setting variables for wing dimensions and flight conditions
-    rho=1.225
-    v=70
+    rho=rho
+    v=v
 
     #The code below fetches the various aerodynamic data from the XFLR analysis
     CL, yspan, Chord, Ai, Cl, ICd, CmAirfquarterchord = ReadingXFLR(file)
@@ -26,12 +26,13 @@ def liftdistribution(file):
     f = interp1d(x,y,kind="cubic", fill_value="extrapolate")
 
     #determining over which range the interpolation needs to be determined, here num= determines the accuracy of the interpolation.
-    xnew = np.linspace(0, 34.96, num=41, endpoint=True)
+    xnew = np.linspace(0, (span/2), num=accuracy, endpoint=True)
 
     #plotting the datapoints and interpolation because it looks nice
     plt.plot(x,y,"o", xnew, f(xnew), "-")
     plt.show()
 
-    return f
+    return f, span/2
 
-liftdistribution('MainWing_a0.00_v10.00ms.csv')
+f, xdist = liftdistribution('MainWing_a0.00_v10.00ms.csv', 1.225, 70, 69.92, 100)
+print(f(9.23))
