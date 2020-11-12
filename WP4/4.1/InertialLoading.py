c2ed1db6be14cf9cec2079cef05c9615cf388e05
Nathan <N.T.Levin@student.tudelft.nl>
2020-11-12 15:29:38 +0100

    defined main

---
+++ WP4/4.1/InertialLoading.py
@@ -0,0 +1,4 @@
+import numpy as np, scipy as sp
+import Project_Github
+
+print(Project_Github.ZFW)
---
+++ WP4/4.1/_main_.py
@@ -0,0 +1,8 @@
+
+#define constants
+
+#MTOW [kg]
+MTOW = 291_509.2
+OEW = 141_412.4
+ZFW = MTOW - OEW
+
