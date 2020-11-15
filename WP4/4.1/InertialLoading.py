fd50dfdeabf5568b250ef27d23422973cefb4e37
Nathan <N.T.Levin@student.tudelft.nl>
2020-11-16 00:38:00 +0100

    define fuel tank

--- WP4/4.1/InertialLoading.py
+++ WP4/4.1/InertialLoading.py
@@ -1,6 +1,37 @@
 import numpy as np, scipy as sp
-import _main_
-MaxFuelWeight = _main_.MaxFuelWeight
+import Constants
 
+
+MaxFuelWeight = Constants.MaxFuelWeight
+MTOW = Constants.MTOW
+EngineWeight = Constants.EngineWeight
+W_uc_MLG = Constants.W_uc_MLG
+
+WingWeight = 24_821.18761
+span = 69.92
+
+def calculateInertialLoading (spanValue, nValue, fuelMass):
+    inertialLoading = WingStructuralWeight(spanValue) +fuelLoading(spanValue. fuelMass)
+
+def WingStructuralWeight (spanValue):
+    #structural weight in [N]
+    localWingWeight = WingWeight -  spanValue / (span /2) * WingWeight
+    return localWingWeight
+
+def fuelLoading (spanValue, fuelMass):
+    #find values
+    D_fuselageOuter = 6.68 #[m]
+    taperRatio = 0.3 #[]
+
+    localFuelWeight = 0
+    beginFuelTank = 2
+    endFuelTank  = 32
+    if spanValue <= beginFuelTank:
+        localFuelWeight =
+
+
+
+
+print(CalculateWingWeight(69.92/2))
 
 
