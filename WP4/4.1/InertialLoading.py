f014d029da9eb5f673dfff346a72ca08a5303923
Nathan <N.T.Levin@student.tudelft.nl>
2020-11-16 14:34:49 +0100

    constants file

--- WP4/4.1/InertialLoading.py
+++ WP4/4.1/InertialLoading.py
@@ -2,10 +2,16 @@ import numpy as np, scipy as sp
 import Constants
 
 
+
 MaxFuelWeight = Constants.MaxFuelWeight
 MTOW = Constants.MTOW
 EngineWeight = Constants.EngineWeight
 W_uc_MLG = Constants.W_uc_MLG
+D_fuselageOuter = Constants.D_fuselageOuter
+taperRatio = Constants.taperRatio
+rootChord= Constants.rootChord
+beginFuelTank = 2
+endFuelTank  = 32
 
 WingWeight = 24_821.18761
 span = 69.92
@@ -20,17 +26,15 @@ def WingStructuralWeight (spanValue):
 
 def fuelLoading (spanValue, fuelMass):
     #find values
-    D_fuselageOuter = 6.68 #[m]
-    taperRatio = 0.3 #[]
-
-    localFuelWeight = 0
-    beginFuelTank = 2
-    endFuelTank  = 32
     if spanValue <= D_fuselageOuter:
-        widthTank
-        localFuelWeight =
+        localFuelWeight = 0
 
 
+
+def localWingboxArea (spanValue):
+    localChord = 0
+    if spanValue <= D_fuselageOuter:
+        localChord =
 
 
 
