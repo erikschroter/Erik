dab5eaab2b7ee5c2bfb42c87a4c72d8aa8f0268f
michielvandm <michielvandermarliere@hotmail.com>
2020-11-16 14:06:17 +0100

    reverting everything back

---
+++ WP4/4.1/intigrate.py
@@ -0,0 +1,52 @@
+<<<<<<< Updated upstream
+=======
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Nov  9 16:21:58 2020
+
+@author: michi
+"""
+
+import numpy as np
+import scipy as sp
+from scipy import integrate
+import matplotlib.pyplot as plt
+
+def ShearForce(LoadingFunction,x,Maxx):
+    return sp.integrate.quad(LoadingFunction,0,x)[0]-sp.integrate.quad(LoadingFunction,0,Maxx)[0]
+    
+def DrawShearForce(LoadingFunction,Maxx):
+    Xlist = [0]
+    Ylist = [0]
+    dt=0.01
+    while Xlist[-1]<=Maxx:
+        Xlist.append(Xlist[-1]+dt)
+        Ylist.append(ShearForce(LoadingFunction,Xlist[-1],Maxx))
+    plt.plot(Xlist,Ylist)
+    plt.title('Shear Force diagram')
+    plt.show()
+    
+def BendingMoment(LoadingFunction,x,Maxx):
+   # def tempfunc(xtemp):
+   #     return ShearForce(LoadingFunction, xtemp, Maxx)
+   # return sp.integrate.quad(tempfunc, 0, x)[0]-sp.integrate.quad(tempfunc, 0, Maxx)[0]
+   return sp.integrate.quad(lambda x: sp.integrate.quad(LoadingFunction,0,x)[0],0,x)[0]-sp.integrate.quad(lambda x: sp.integrate.quad(LoadingFunction,0,x)[0],0,Maxx)[0]
+    
+ 
+    
+def DrawBendingMoment(LoadingFunction,Maxx):
+    Xlist = [0]
+    Ylist = [0]
+    dt=0.01
+    while Xlist[-1]<=Maxx:
+        Xlist.append(Xlist[-1]+dt)
+        Ylist.append(BendingMoment(LoadingFunction,Xlist[-1],Maxx))
+    plt.plot(Xlist,Ylist)
+    plt.title('Bending Moment diagram')
+    plt.show()   
+    
+
+
+
+
+>>>>>>> Stashed changes
