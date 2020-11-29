b70fd34beb4fa3a32eb6d80dd2f1375f724da851
Nathan <N.T.Levin@student.tudelft.nl>
2020-11-29 22:53:55 +0100

    archived simplifiedinertialloading.py

--- WP4/4.1/Constants.py
+++ WP4/4.1/Constants.py
@@ -1,7 +1,7 @@
-MaxFuelWeight = 0
-MTOW = 0
-EngineWeight = 0
-W_uc_MLG = 0
+MaxFuelWeight = 143_241.5525 #kg
+MTOW = 304_636.2789 #kg
+EngineWeight = 20_487.986 #kg
+W_uc_MLG = 7_569.349414 #kg
 D_fuselageOuter = 6.68 #[m]
 taperRatio = 0.3 #[]
 rootChord = 11.95 [m]
