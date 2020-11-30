d4d22b70e3f32debb9fdbdf293892d4075b79579
ChristophPabsch <cmpabsch@gmail.com>
2020-11-30 15:14:08 +0100

    Update CrackPropagation.py

--- WP5/CrackPropagation.py
+++ WP5/CrackPropagation.py
@@ -9,7 +9,9 @@ import math as m
 
 
 def TensilePropagationStress(c):
-    K_1c = 29 * 10 ^ 6  # Pa, material property of 
+    K_1c = 29 * 10 ^ 6  # Pa, material property of
     Y = 1
     Tensile_Propagation_Stress = K_1c / (Y * m.sqrt(m.pi * c))
     return Tensile_Propagation_Stress
+
+print(TensilePropagationStress(0.005))
