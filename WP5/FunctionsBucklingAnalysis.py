eddfb5dc0a42be1b2b23e605fc2253f283b7a5b3
erikschroter <erikschroter@gmail.com>
2020-11-30 15:08:36 +0100

    Update FunctionsBucklingAnalysis.py

--- WP5/FunctionsBucklingAnalysis.py
+++ WP5/FunctionsBucklingAnalysis.py
@@ -6,3 +6,14 @@ Created on Mon Nov 30 14:53:19 2020
 """
 
 
+# Margin of Safety Function
+def MoSdef(failure_stress, applied_stress):
+    MoS = failure_stress / applied_stress
+    if MoS < 1:
+        print("\n !!! STRUCTURAL FAILURE !!!")
+    return MoS
+
+# Shear Buckling
+# Skin Buckling
+# Column Buckling
+# Compressive strength failure
