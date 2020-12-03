a3fc215a65b156ba2a3deab2d28d5d747da646ce
PhilipVaartjes <PhilipVaartjes@users.noreply.github.com>
2020-12-03 16:14:59 +0100

    Skin Buckling Analysis added to eriks file

--- WP5/NormalForceDueToBending.py
+++ WP5/NormalForceDueToBending.py
@@ -1,18 +1,20 @@
+import os
+import sys
+directory = os.path.dirname(__file__)+"\\5.1"
+sys.path.insert(-1,directory)
 
+from Moment_of_Inertia import Ixx_in_y
 
 #Max moment occurs at MZFW and n = 4.65 (the wing wants to bend upwards so the resulting moment in the root is negative but counterclockwise positive)
 
 M_max = -45430675 #Nm CCW+ if seen from the back of the airplane
-
-def Sigma_z(M_x, I_xx, I_yy,y):
-    Sigma_z = (M_x * I_yy * y)/(I_xx*I_yy)
-    return Sigma_z
-
-#y_max at max distance from centre line
-
-y_max = 0.97512 #This is a
+y_max = -0.97512 #This is an estimate fix it later
 
 def Force_max(M_x, y_max, I_xx):
     Force_max =  (M_x * y_max)/(I_xx)
     return Force_max
 
+I_xx = Ixx_in_y(0)
+
+Force_max= Force_max(M_max, y_max, I_xx)
+print(Force_max)
