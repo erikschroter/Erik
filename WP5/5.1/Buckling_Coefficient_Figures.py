35030f7261213970af628b7b0be91adc73daf22c
ChristophPabsch <cmpabsch@gmail.com>
2020-12-07 17:01:23 +0100

    Update Buckling_Coefficient_Figures.py

--- WP5/5.1/Buckling_Coefficient_Figures.py
+++ WP5/5.1/Buckling_Coefficient_Figures.py
@@ -23,14 +23,15 @@ for i in range(len(hinged_edges)):
     y_hinged.append(hinged_edges[i][1])
 hinged_edges_function = sp.interpolate.interp1d(x_hinged, y_hinged, kind="quadratic", fill_value="extrapolate")
 
-figure_19_a_clamped = [(1, 15), (1.1, 13.8), (1.2, 13), (1.25, 12.5), (1.5, 11.5), (1.66, 11.3), (1.75, 10.7), (2, 10.3),
-                 (2.25, 10.1), (2.5, 9.9), (2.75, 9.8), (3, 9.8), (3.5, 9.7), (4, 9.6), (5, 9.6)]
+figure_19_a_clamped = [(0.7, 15.1), (0.8, 12), (0.9, 10.4), (1, 10.3), (1.1, 10.4), (1.3, 9), (1.5, 8.5), (1.65, 8.3),
+                 (1.8, 8.4), (1.9, 8.15), (2, 8), (2.2, 7.8), (2.4, 7.8), (2.6, 7.65), (3, 7.55), (3.4, 7.45), (3.7, 7.4),
+                        (4, 7.3), (4.3, 7.4), (4.6, 7.3),(5, 7.2)]
 x_a_clamped = []
 y_a_clamped = []
-for i in range(len(clamped_edges)):
-    x_a_clamped.append(clamped_edges[i][0])
-    y_a_clamped.append(clamped_edges[i][1])
-clamped_edges_function = sp.interpolate.interp1d(x_a_clamped, y_a_clamped, kind="quadratic", fill_value="extrapolate")
+for i in range(len(figure_19_a_clamped)):
+    x_a_clamped.append(figure_19_a_clamped[i][0])
+    y_a_clamped.append(figure_19_a_clamped[i][1])
+figure_19_a_clamped_function = sp.interpolate.interp1d(x_a_clamped, y_a_clamped, kind="quadratic", fill_value="extrapolate")
 
 
 def clamped_edges_callable_function(x):
@@ -41,9 +42,12 @@ def hinged_edges_callable_function(x):
     return hinged_edges_function(x)
 
 
+def figure_19_a_clamped_callable_function(x):
+    return figure_19_a_clamped_function(x)
+
+
 """
-plt.plot(x_hinged, clamped_edges_function(x_hinged), "-")
-plt.plot(x_hinged, hinged_edges_function(x_hinged), "-")
+plt.plot(x_a_clamped, figure_19_a_clamped_function(x_a_clamped), "-")
 
 # plot formatting
 
@@ -55,4 +59,5 @@ plt.ylabel('C_y [mm]')
 plt.grid(True, which='both')
 plt.axhline(y=0, color='k')
 
-plt.show()"""
+plt.show()
+"""
